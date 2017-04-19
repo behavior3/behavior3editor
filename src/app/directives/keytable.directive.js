1494d4dba1f7461e58c1aede5420d9e6a81e4adf
bobismijnnaam <bobrubbens@gmail.com>
2017-04-19 16:10:21 +0200

    added red highlighting! only on the root it's broken now, but that's okay.

--- src/app/directives/keytable.directive.js
+++ src/app/directives/keytable.directive.js
@@ -64,7 +64,7 @@
     }
 
     function add(key, value, fixed) {
-      vm.rows.push({key:key, value:value, fixed:fixed===true});
+      vm.rows.push({key:key, value:value, valueType:getValueType(value), fixed:fixed===true, extra_css:""});
     }
 
     function remove(i) {
@@ -84,9 +84,11 @@
         if (! r.key) continue;
 
         var value = r.value;
+        var oldValue = r.value;
         if (!isNaN(value) && value !== '') {
           value = parseFloat(value);
         }
+        var newValue = value;
 
         vm.model[r.key] = value;
         
@@ -95,14 +97,54 @@
         //if (vm._onChange) {
           //vm._onChange($scope);
         //}
+        
+        r.valueType = getValueType(value);
+
+        if (oldValue != String(newValue)) {
+          r.extra_css = "background-color: red;";
+        } else {
+          r.extra_css = "";
+        }
       }
 
-      // This is to make sure if there are no more keys, the empty model
+      // This also is to make sure if there are no more keys, the empty model
       // is correctly propagated to the place where the models are stored
       if (vm._onChange) {
         vm._onChange($scope);
       }
     }
+  }
+
+  function getValueType(value) {
+    if (value == "true") {
+      return "BOL";
+    } else if (value == "false") {
+      return "BOL";
+
+    } else if (((typeof value) === "string")
+              && value.length > 0
+              && value.slice(-1) == "d"
+              && numberBeforeD(value)
+              ) {
+      return "DBL";
+    } else if (!isNaN(value) && value !== '') {
+      var valuePrime = parseFloat(value);
+      var strValue = String(valuePrime);
+
+      if (/\./.test(strValue)) {
+        return "DBL";
+      } else {
+        return "INT";
+      }
+    } else {
+      return "STR";
+    }
+  }
+
+  function numberBeforeD(value) {
+    var shortValue = value.substring(0, value.length - 1);
+
+    return !isNaN(shortValue);
   }
 
 })();
